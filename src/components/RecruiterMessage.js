250db0dd631d1ae2c256b446eda7b6e3212648e3
aduterte <alexduterte@gmail.com>
2020-08-18 22:26:20 -0400

    added email link for recruiters

--- src/components/RecruiterMessage.js
+++ src/components/RecruiterMessage.js
@@ -8,43 +8,56 @@ export default function RercruiterMessage(props){
     [location, setLocation] = useState(""),
     [tidbit, setTidbit] = useState(""),
     [question, setQuestion] = useState(""),
+    [email, setEmail] = useState(""),
     warning = {color:"red",fontWeight:"bold"}
 
-function handleContact(e){
-    const contact=e.target.value
-    setContact(contact)
-}
+    const message = `Hi ${contact},%0D%0AI hope you are doing well. My name is ${props.user} and I am a recent graduate of Flatiron School's Software Engineering Program.  I came across the ${position} position in ${location} and after learning more about ${company}, I was very intrigued by ${tidbit}.%0D%0A%0D%0AI would love to schedule a virtual coffee soon so I could learn more about what it's like to work at ${company} and ask a few questions about ${question}. If your schedule doesn’t permit us to connect, would you be open to answering a few questions via email?%0D%0A%0D%0AI look forward to hearing from you soon. Thanks in advance!%0D%0A%0D%0ABest,%0D%0A${props.user}<img src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/linkedin-icon-2x.png"/>
+    `
 
-function handleCompany(e){
-    const company = e.target.value
-    setCompany(company)
-}
+ 
 
-function handlePosition(e){
-    const position = e.target.value
-    setPosition(position)
-}
+    function handleContact(e){
+        const contact=e.target.value
+        setContact(contact)
+    }
 
-function handleLocation(e){
-    const location = e.target.value
-    setLocation(location)
-}
+    function handleCompany(e){
+        const company = e.target.value
+        setCompany(company)
+    }
 
-function handleTidbit(e){
-    const tidbit = e.target.value
-    setTidbit(tidbit)
-}
+    function handlePosition(e){
+        const position = e.target.value
+        setPosition(position)
+    }
 
-function handleQuestion(e){
-    const question = e.target.value
-    setQuestion(question)
-}
+    function handleLocation(e){
+        const location = e.target.value
+        setLocation(location)
+    }
+
+    function handleTidbit(e){
+        const tidbit = e.target.value
+        setTidbit(tidbit)
+    }
+
+    function handleQuestion(e){
+        const question = e.target.value
+        setQuestion(question)
+    }
+
+    function handleEmail(e){
+        const email = e.target.value
+        setEmail(email)
+    }
 
 
     return(
         <div className="recruiter-message-container">
             <h2>Recruiter Message</h2>
-          
+                <br/>
+                <label>Enter Recruiter's E-mail </label><input type="text" value={email} onChange={handleEmail} placeholder="name@somewhere.com"/>
+                <br/>
                 <label>Enter Recruiter's Name </label><input type="text" value={contact} onChange={handleContact} placeholder="Enter Recruiters Name"/>
                 <br/>
                 <label>Enter Company </label><input type="text" value={company} onChange={handleCompany} placeholder="Enter Company Name"/>
@@ -56,14 +69,12 @@ function handleQuestion(e){
                 <label>Enter Company Tidbit </label><input type="text" value={tidbit} onChange={handleTidbit} placeholder="Enter Tidbit"/>
                 <br/>
                 <label>Enter Question</label><input type="text" value={question} onChange={handleQuestion} placeholder="Enter what you would like to speak about, e.g. keys to success"/>
-                <br/>
                 
-                {/* <label>Enter Your Name</label><input type="text" value={user} onChange={handleUser} placeholder="Enter Your Name"/> */}
            
             <p><b>Quick Question about {company === "" ? <span style={warning}> * Company Missing * </span> : company}</b></p>
             <p>Hi {contact === "" ? <span style={warning}> * Name Missing * </span> : contact}</p>
 
-            <p>I hope you are doing well.  My name is {props.user === "" ? <span style={warning}> * Your Name Missing * </span> : props.user} and I am a recent graduate of Flatiron School's Software Engineering Program.  I came across the {position === "" ? <span style={warning}> * Position Missing * </span> : position} in {location === "" ? <span style={warning}> * location Missing * </span> : location} and after learning more about {company === "" ? <span style={warning}> * Company Missing * </span> : company}, I was very intrigued by {tidbit === "" ? <span style={warning}> * what caught your eye * </span> : tidbit}.</p>
+            <p>I hope you are doing well.  My name is {props.user === "" ? <span style={warning}> * Your Name Missing * </span> : props.user} and I am a recent graduate of Flatiron School's Software Engineering Program.  I came across the {position === "" ? <span style={warning}> * Position Missing * </span> : position} position in {location === "" ? <span style={warning}> * location Missing * </span> : location} and after learning more about {company === "" ? <span style={warning}> * Company Missing * </span> : company}, I was very intrigued by {tidbit === "" ? <span style={warning}> * what caught your eye * </span> : tidbit}.</p>
 
 
             <p>I would love to schedule a virtual coffee soon so I could learn more about what it's like to work at {company === "" ? <span style={warning}> * Company Missing * </span> : company} and ask a few questions about {question === "" ? <span style={warning}> * your question * </span> : question}. If your schedule doesn’t permit us to connect, would you be open to answering a few questions via email?</p>
@@ -72,6 +83,8 @@ function handleQuestion(e){
             <p>Best,
             <br/>
             {props.user === "" ? <span style={warning}> * Your Name Missing * </span> : props.user}</p>
+
+            <a href={`mailto:${email}?subject=Quick Question about ${company}&html-body=${message}`} target="_blank" rel="noopener noreferrer">E-mail it</a>
             
         </div>
     )
