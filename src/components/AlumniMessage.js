7344f1f271ea7b62a15a5923be479227be486b2c
aduterte <alexduterte@gmail.com>
2020-08-19 01:19:37 -0400

    added alumni copy to clipboard

--- src/components/AlumniMessage.js
+++ src/components/AlumniMessage.js
@@ -16,9 +16,32 @@ export default function AlumniMessage(props){
         setCompany(company)
     }
 
+    function handleCopy(){
+        console.log("im hit")
+        
+        let bodyText = document.getElementById("alumni-text"),
+            selection = window.getSelection(),
+            range = null
+
+
+            range = document.createRange()
+        range.selectNode(bodyText)
+        selection.removeAllRanges()
+        selection.addRange(range)
+        
+       
+        document.execCommand('copy')
+        
+        // debugger
+        
+        selection.removeAllRanges()
+        
+        range = null
+    }
     
     return(
         <div className="alumni-message-container">
+           
             <h2>Alumni LinkedIn Message</h2>
           
                 <label>Enter Alumni's Name</label><input type="text" value={contact} onChange={handleContact} placeholder="Enter Alumni Name"/>
@@ -26,7 +49,7 @@ export default function AlumniMessage(props){
                 <label>Enter Alumni's Company</label><input type="text" value={company} onChange={handleCompany} placeholder="Enter Alumni Company"/>
                 <br/>
                 {/* <label>Enter Your Name</label><input type="text" value={user} onChange={handleUser} placeholder="Enter Your Name"/> */}
-           
+            <div id="alumni-text">
             <p><b>Greetings from a fellow Flatiron Alumni</b></p>
             <p>Hi {contact === "" ? <span style={warning}> * Name Missing * </span> : contact}</p>
             <p>I hope all is well with you.  I noticed that you attended Flatiron School and began your new journey as a developer at {company === "" ? <span style={warning}> * Company Missing * </span> : company}.  As a recent Flatiron School graduate, I would love to know more about your post-graduation experience and how you successfully transition into the industry.</p>
@@ -35,7 +58,10 @@ export default function AlumniMessage(props){
             <p>Best,
             <br/>
             {props.user === "" ? <span style={warning}> * Name Missing * </span> : props.user}</p>
-            
+            </div>
+            <div>
+                <button onClick={handleCopy}>Click to Copy Message</button>
+            </div>
         </div>
     )
 }
