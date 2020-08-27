849d4b6c43ae7c753bbc4c4b5d21f6eb3a1b9cbf
aduterte <alexduterte@gmail.com>
2020-08-27 17:21:38 -0400

    fixed typos

--- src/components/RecruiterContact.js
+++ src/components/RecruiterContact.js
@@ -53,7 +53,7 @@ export default function RecruiterContact(props){
                 <button onClick={handleReset}>Reset</button>
             <div id="recruiter-text">
             <p>Hi {contact.firstName === "" ? <span style={warning}> * Name Missing * </span> : contact.firstName},</p>
-            <p>My Name is {props.user === "" ? <span style={warning}> * Your Name Missing * </span> : props.user}. I'm a Software Engineer. If you have a second, I would love the chance to discuss how my developer skills and experiences might match the {position === "" ? <span style={warning}> * Position Missing * </span> : position} I’ve applied for. Thanks for connecting and I hope you have a great day! </p>
+            <p>My Name is {props.user === "" ? <span style={warning}> * Your Name Missing * </span> : props.user}. I'm a Software Engineer. If you have a second, I would love the chance to discuss how my developer skills and experiences might match the {position === "" ? <span style={warning}> * Position Missing * </span> : position} role I’ve applied for. Thanks for connecting and I hope you have a great day! </p>
             </div>
             <div>
                 <button onClick={handleCopy}>Click to Copy Message</button>
